60bc6d2749ec1a8cda04e800494ffbbdeed7e9d8
David Islam <david.islam24@gmail.com>
2022-05-19 21:49:11 -0700

    Complete Lesson 13

--- validation.js
+++ validation.js
@@ -1,3 +1,29 @@
+/* Each property of this object corresponds 
+to the name of an input field in the HTML. */
 const patterns = {
     telephone: /^\d{11}$/,
+    username: /^$/,
+    email: /^$/,
+    password: /^$/,
+    slug: /^$/,
 }
+
+const inputs = document.querySelectorAll('input');
+
+/* Validates the node <inputField> against 
+the regular expression <regex>. */
+function validate(inputField, regex) {
+    if (regex.test(inputField.value)) {
+        inputField.className = 'valid'; 
+    } else {
+        inputField.className = 'invalid';
+    }
+}
+
+inputs.forEach(input => {
+    input.addEventListener('keyup', e => {
+        const inputName = e.target.attributes.name.value;
+        const regex = patterns[inputName];
+        validate(e.target, regex); 
+    })
+})
